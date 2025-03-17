67ba7b82226b58dbd4be00a40f07d271a4fd9841
Aditya Singh <singhaditya4333@gmail.com>
2025-03-17 09:03:34 +0530

    connect database

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,35 @@
+//easy and straight in express etc. but in nextjs its a edge time framework.
+// as user requests it runs. not all time running.
+
+//hamesa check karenge ki pehele se db connected hai ya nahi aur then connect
+// karenge always. wrna aaplication choke ho jaegi.
+
+import { promises } from "dns";
+import mongoose from "mongoose";
+
+type connectionObject = {  //typescript. check on type of connectionObject
+    isConnected ?: number
+}
+
+const connection: connectionObject = {} //connection is connectionObject type. initialised as empty
+
+async function dbConnect() : Promise<void>{ //function will return a promise and its datatime doesn't matter (void)
+    if(connection.isConnected){
+        console.log("Already connected to database")
+        return
+    }
+
+    try {
+        const db = await mongoose.connect(process.env.MONGODB_URI || "", {}) 
+        
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("DB connected successfully.")
+    } catch (error) {
+        
+        console.log("DB connection failed.", error)
+        process.exit(1)
+    }
+}
+
+export default dbConnect
